dc0dca42fae653d3f5ac9b943a7bb0210df72a23
Michele Ferretti <mic.ferretti@gmail.com>
2017-01-21 17:12:46 +0000

    add more steps to stepper-test.js

--- stepper-test.js
+++ stepper-test.js
@@ -13,14 +13,44 @@ board.on("ready", function() {
    */
 
   var stepper = new five.Stepper({
-    type: five.Stepper.TYPE.DRIVER,
-    stepsPerRev: 513,
-    pins: [8,10]
+    type: five.Stepper.TYPE.FOUR_WIRE,
+    stepsPerRev: 32,
+    pins: [8,9,10,11]
     // pins: {
     //   step: 11,
     //   dir: 13
     // }
   });
+
+  var buttonRight = new five.Button(12);
+  var buttonLeft = new five.Button(2);
+  var led = new five.Led(13);
+
+
+  buttonRight.on("press", function() {
+   console.log( "Stepping RIGHT" );
+   led.on();
+   stepper.direction(0).step(200, function() {
+     console.log("Done stepping!");
+   });
+ });
+
+ buttonRight.on("release", function() {
+   led.off();
+ });
+
+ buttonLeft.on("press", function() {
+   console.log( "Stepping LEFT" );
+   led.on();
+   stepper.direction(1).step(200, function() {
+     console.log("Done stepping!");
+   });
+});
+
+buttonLeft.on("release", function() {
+  led.off();
+});
+
 
   // Make 10 full revolutions counter-clockwise at 180 rpm with acceleration and deceleration
   // stepper.rpm(180).ccw().accel(1600).decel(1600).step(2000, function() {
@@ -37,7 +67,14 @@ board.on("ready", function() {
   //   });
   // });
 
-  stepper.rpm(180).ccw().step(200, function(){
-   console.log("done");
-  });
+  // stepper.rpm(180).ccw().step(200, function(){
+  //  console.log("done");
+  // });
+//   stepper.direction(five.Stepper.DIRECTION.CW).step(2000, function() {
+//   console.log("Done stepping!");
+// });
+
+// stepper.direction(five.Stepper.DIRECTION.CCW).step(2000, function() {
+//   console.log("Done stepping!");
+// });
 });
